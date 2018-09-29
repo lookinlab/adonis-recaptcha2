5f1e2bc117ae8e2545b2925f35a16b368405555a
LookinGit <alook.in@yandex.ru>
2018-09-29 16:35:31 +0500

    chore(package): update dependencies

---
+++ japaFile.js
@@ -0,0 +1,15 @@
+'use strict'
+
+/*
+ * adonis-recaptcha2
+ *
+ * (c) Lookin Anton <lookin@lookinlab.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+const { configure } = require('japa')
+configure({
+  files: ['test/*.spec.js']
+})
